1a4a457a7c17a17fd09df3f8bef7318ec83f2239
Андрей Чуфырев <andrei4.2008@gmail.com>
2019-01-14 20:47:53 +0300

    Initial Commit

---
+++ trackpad-joystick/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  trackpad-joystick
+//
+//  Created by Андрей Чуфырев on 14/01/2019.
+//  Copyright © 2019 ussserrr. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ trackpad-joystick/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  trackpad-joystick
+//
+//  Created by Андрей Чуфырев on 14/01/2019.
+//  Copyright © 2019 ussserrr. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
